465bdadb8ca7d60a8b301c5fbd1740430766a504
Martin Zink <martin.zink@apache.org>
2025-01-24 17:01:38 +0100

    asd3

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ fpaths = [os.path.join(v[pv], v['LDLIBRARY']) for pv in ('LIBDIR', 'LIBPL')];
 for fpath in fpaths:
     so_paths = glob.glob(f"{fpath}*")
     for so_path in so_paths:
+        print(so_path)
         if os.path.exists(so_path):
-            print(so_path)
-            exit(0)
-exit(1)
+            print(f"FOUND IT {so_path}")

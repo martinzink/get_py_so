0cdf5bd3af22d0439a7a51e033b1a5afc3dd616e
Martin Zink <martin.zink@apache.org>
2025-01-24 17:28:15 +0100

    asd8

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 import sysconfig, os, glob
 
 v = sysconfig.get_config_vars();
-possible_dirs_between = ['']
-fpaths = [os.path.join(v['LIBDIR'], pdir, v['LDLIBRARY']) for pdir in possible_dirs_between]
-print(fpaths)
-for fpath in fpaths:
-    so_paths = glob.glob(f"{fpath}*")
-    print(so_paths)
-    for so_path in so_paths:
-        print(so_path)
-        if os.path.exists(so_path):
-            print(f"FOUND IT {so_path}")
+
+so_paths = glob.glob(f"{v['LIBDIR']}/*{v['LDLIBRARY']}*")
+print(so_paths)
+for so_path in so_paths:
+    print(so_path)
+    if os.path.exists(so_path):
+        print(f"FOUND IT {so_path}")

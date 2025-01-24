4de49b8b150043672c35853fa023e530b731f80d
Martin Zink <martin.zink@apache.org>
2025-01-24 16:59:59 +0100

    asd2

--- main.py
+++ main.py
@@ -1,18 +1,11 @@
-import sysconfig
-import os
-import glob
+import sysconfig, os, glob
 
-v = sysconfig.get_config_vars()
-so_name = v['LDLIBRARY']
-libdir = v['LIBDIR']
-libpl = v['LIBPL']
-libdir_so = os.path.join(libdir, so_name)
-libpl_so = os.path.join(libpl, so_name)
-
-print(libdir_so)
-print(os.path.exists(libdir_so))
-print(libpl_so)
-print(os.path.exists(libpl_so))
-
-print("OLD")
-print(min(glob.glob(os.path.join(v['LIBDIR'], f"libpython{sysconfig.get_config_var('VERSION')}.so*")), key=len, default=''))
+v = sysconfig.get_config_vars();
+fpaths = [os.path.join(v[pv], v['LDLIBRARY']) for pv in ('LIBDIR', 'LIBPL')];
+for fpath in fpaths:
+    so_paths = glob.glob(f"{fpath}*")
+    for so_path in so_paths:
+        if os.path.exists(so_path):
+            print(so_path)
+            exit(0)
+exit(1)

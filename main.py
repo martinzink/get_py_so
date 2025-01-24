7d8ab2d9702ce7ffdb6c989f963eb16ea16e5f6a
Martin Zink <martin.zink@apache.org>
2025-01-24 17:12:58 +0100

    asd6

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ fpaths = [os.path.join(v['LIBDIR'], pdir, v['LDLIBRARY']) for pdir in possible_d
 print(fpaths)
 for fpath in fpaths:
     so_paths = glob.glob(f"{fpath}*")
+    print(so_paths)
     for so_path in so_paths:
         print(so_path)
         if os.path.exists(so_path):

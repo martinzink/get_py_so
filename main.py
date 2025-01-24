e4aa67af19ac29cad1fba75cb1bcc5fedb2f1364
Martin Zink <martin.zink@apache.org>
2025-01-24 17:35:15 +0100

    asd10

--- main.py
+++ main.py
@@ -2,7 +2,13 @@ import sysconfig, os, glob
 
 v = sysconfig.get_config_vars();
 
-so_paths = glob.glob(f"{v['LIBDIR']}/**{v['LDLIBRARY']}*", recursive=True)
+so_paths = glob.glob(f"{v['LIBDIR']}/*{v['LDLIBRARY']}*")
+print(so_paths)
+for so_path in so_paths:
+    print(so_path)
+    if os.path.exists(so_path):
+        print(f"FOUND IT {so_path}")
+so_paths = glob.glob(f"{v['LIBDIR']}/*/*{v['LDLIBRARY']}*")
 print(so_paths)
 for so_path in so_paths:
     print(so_path)
